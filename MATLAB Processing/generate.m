3a3d3dcad1536deaccbaf42ec9b43483fc1e3f8a
Cristian Reyes Rodríguez <cristianreyesrodriguez@gmail.com>
2015-02-26 12:13:00 +0100

    Generador

---
+++ MATLAB Processing/generate.m
@@ -0,0 +1,47 @@
+function [ output_args ] = generate(v, t0 ,SNR, n)
+%GENERATE2 Summary of this function goes here
+%   n = 0, no noise. n=1, adds noise.
+f0 = 10.525e9;
+c = 3e8;
+N0 = 0.5;
+A = sqrt(10^(SNR/10)*N0); %Amplitud de senyal
+Fs=10e3; %Hz
+T=10; %segons
+t = 0:1/Fs:T;
+x = normrnd(0,N0,1,length(t))*n;
+
+lambda = c/f0;
+fd = -2/lambda.*v;
+d = 12./v;
+smoothness = 1/3;
+for i = 1:length(d)
+    u(i,:) = (atan((t-t0(i)+d(i)/2)/smoothness) + pi/2)/pi - (atan((t-t0(i)-d(i)/2)/smoothness) + pi/2)/pi; 
+    u(i,:) = u(i,:)/max(u(i,:)); %Normalització de la potència
+    
+end
+figure(10);   
+plot(t,u);
+a = A*u;
+
+for i= 1:length(v)
+    x = x+a(i,:).*sin(2*pi*fd(i)*(t>=t0(i)-d(i)/2).*(t<=t0(i)+d(i)/2).*t);
+end
+figure(1)
+plot(t,x);
+figure(2)
+plot(abs(fft(x)))
+figure(1);
+subplot(2,1,1)
+spectrogram(x,256,128,256,Fs);
+view([0 0]);
+title('Power vs Frequency')
+
+subplot(2,1,2)
+spectrogram(x,256,128,256,Fs);
+view([90 0]);
+title('Power vs Time')
+
+sound(x,Fs);
+
+end
+
